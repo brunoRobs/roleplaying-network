44732b45e6765af9999e94d9f85b480a4691d64d
brunoRobs <git.brunoRobs.excal@gmail.com>
2025-03-12 11:01:46 -0300

    Campaigns list added

---
+++ src/components/campaigns/Campaigns.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="campaigns" v-if="campaigns.length">
+    <div id="list-container">
+      <ul id="list">
+        <li v-for="(campaign, index) in campaigns.map(campaign => ({ name: campaign.name, id: campaign.id }))"
+          @click="select($event, campaign.id)" :class="!index ? 'selected' : ''">
+          {{ `${campaign.name.length > 12 ? campaign.name.substring(0, 12) + '...' : campaign.name}` }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        target: undefined,
+        campaignId: undefined,
+        campaigns: []
+      }
+    },
+    computed: {
+      campaign() {
+        return this.campaigns.find(campaign => campaign.id === this.campaignId);
+      }
+    },
+    methods: {
+      select(event, id) {
+        if (this.target) this.target.classList.remove('selected');
+        this.target = event.target;
+        this.campaignId = id;
+        this.target.classList.add('selected');
+      }
+    },
+    beforeMount() {
+      this.campaignId = this.campaigns[0].id;
+    },
+    mounted() {
+      const array = Array.from(document.getElementsByClassName('list')[0].children)
+      this.target = array.find(item => item.classList.contains('selected'));
+    }
+  }
+</script>
+
+<style scoped>
+  #campaigns {
+    display: flex;
+    background: rgb(255, 99, 71);
+    width: 100%;
+    height: 100%;
+  }
+
+  #list-container {
+    background: rgb(180, 71, 52);
+    width: 17%;
+    height: 100%;
+    overflow-y: auto;
+  }
+
+  #list {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    margin: 10% 0 10% 10%;
+  }
+
+  #list li {
+    list-style: none;
+    cursor: pointer;
+    width: 80%;
+    padding: 5% 0 5% 5%;
+    border-radius: 8px;
+    font-size: 1.5em;
+  }
+
+  #list li.selected {
+    background-color: rgb(230, 121, 102);
+  }
+
+  #list li:hover {
+    background-color: rgb(230, 121, 102);
+  }
+</style>
