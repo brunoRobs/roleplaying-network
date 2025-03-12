c9ce2bfeb729122faeb12870b81ea6dea51905e9
brunoRobs <git.brunoRobs.excal@gmail.com>
2025-03-12 12:13:45 -0300

    Footer default structure

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,7 +1,14 @@
-<template></template>
+<template>
+  <div id="footer">Footer</div>
+</template>
 
 <script>
   export default {}
 </script>
 
-<style scoped></style>
+<style scoped>
+  #footer {
+    height: 92px;
+    border-top: 4px solid rgb(0, 0, 0);
+  }
+</style>
