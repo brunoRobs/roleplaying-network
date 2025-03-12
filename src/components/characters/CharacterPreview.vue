0e4db2327a4922bdc17168b1339591c95c2967e6
brunoRobs <git.brunoRobs.excal@gmail.com>
2025-03-12 19:00:33 -0300

    Characters list and preview

---
+++ src/components/characters/CharacterPreview.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="character-preview">
+    <img class="image" :src="character.image" alt="Personagem" @click="show">
+    <div class="infos" @click="show">
+      <p class="name">{{ character.name }}</p>
+      <p class="class-level">{{ `${character.class} ${character.level}` }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      character: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      show() {
+        this.$emit('show', this.character.cid);
+      }
+    },
+    emits: ['show']
+  }
+</script>
+
+<style scoped>
+  .character-preview {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+    margin: 16px;
+  }
+
+  .character-preview * {
+    cursor: pointer;
+  }
+
+  p {
+    width: fit-content;
+  }
+
+  .image {
+    height: 64px;
+    border-radius: 32px;
+  }
+
+  .infos {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    flex-direction: column;
+  }
+
+  .name {
+    font-size: 1em;
+  }
+
+  .class-level {
+    font-size: .75em;
+  }
+</style>
---
+++ src/components/characters/Characters.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="characters" v-if="characters.length">
+    <div id="characters-container">
+      <div class="character" v-for="character in characters.map(character => (
+        {
+          cid: character.cid,
+          name: character.sheet.name,
+          class: character.sheet.class,
+          level: character.sheet.level,
+          image: character.sheet.image
+        }
+      ))">
+        <CharacterPreview :character="character" />
+      </div>
+    </div>
+  </div>
+  <div v-else>
+    {{ 'Nenhum personagem encontrado' }}
+  </div>
+</template>
+
+<script>
+  import CharacterPreview from './CharacterPreview.vue';
+
+  export default {
+    components: { CharacterPreview },
+    data() {
+      return {
+        characters: []
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #characters {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgb(255, 99, 71);
+    width: 100%;
+    height: 100%;
+  }
+
+  #characters-container {
+    display: grid;
+    grid-template-columns: repeat(5, 20%);
+    grid-template-rows: repeat(auto-fit, auto);
+    max-height: 80%;
+    width: 80%;
+    overflow-y: auto;
+    border-radius: 32px;
+    background-color: rgb(255, 255, 255, .75);
+
+  }
+
+  .character {
+    height: fit-content;
+  }
+
+  #characters-container::-webkit-scrollbar {
+    display: none;
+  }
+</style>
